877980d0ce5f267eccd54fae20136f648828dfa5
ChinweRose <chinwendurosemary30@gmail.com>
2022-10-03 18:45:27 +0100

    fixed username if invalid

--- src/divineTest/PrintName.java
+++ src/divineTest/PrintName.java
@@ -2,5 +2,15 @@ package divineTest;
 
 public class PrintName {
 
+    public String printUsername(String username){
+        if (username.isEmpty()){
+            System.out.println("Please give me a username");
+        }
+        else {
+            System.out.println("Hello " + username);
+        }
+        return username;
+    }
+
 
 }
